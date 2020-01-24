14c093e24d7dd72f86a25bfe14d3dd1d3b28593f
ihavehands <zaneshook@gmail.com>
2020-01-24 04:06:14 -0800

    models

---
+++ models/forums.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  content: String,
+  timestamps: true
+});
+
+const fourmSchema = new Schema({
+  name: String,
+  subject: String,
+
+  comments: [{ commentSchema }],
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ]
+});
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
+
+require('dotenv').config()
+require('./config/passport')
+require('./config/database')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -18,9 +25,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser())
+app.use(
+  session({
+    secret: 'SEI-DT-69-Rocks!',
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
